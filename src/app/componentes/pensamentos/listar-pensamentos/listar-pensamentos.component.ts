380b776533a393fd3a4c47c2243ca6ba9eaaf87c
Cameasy <camila.correia662@gmail.com>
2023-04-12 12:54:37 -0300

    atualizando o projeto

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam finibus lacinia dui quis imperdiet. Donec eu massa lorem. Ut molestie, tellus id blandit ultricies, tortor odio consequat metus, sit amet placerat elit magna a quam. Nam eget venenatis est. Quisque eget justo fermentum, fringilla felis at, malesuada urna. Mauris quis sem magna. Vestibulum at eros quis diam pharetra egestas sit amet et sapien. Curabitur vitae interdum orci. Praesent vel vulputate dui. Phasellus luctus tortor dolor, sed blandit nulla finibus at. Vivamus pellentesque a neque ac dictum. Donec lacinia lectus a quam feugiat euismod. Pellentesque eleifend blandit aliquam. Pellentesque id ipsum id massa pretium luctus nec feugiat orci. Donec risus risus, mollis quis nisi vitae, vestibulum lobortis massa',
+      autoria: 'Camesis',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Passo informações por um preço amigável',
+      autoria: 'Camesis',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Pipipipopopo existe sim!',
+      autoria: 'Camesis',
+      modelo: 'modelo2'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
